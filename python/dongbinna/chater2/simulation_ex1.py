e35e8f4533cb8f8bc25f951a8e9c5ce81deb9394
tjdwp0211 <rlatjdwp0211@naver.com>
2025-01-31 15:22:20 +0900

    [lesson] dingbinna simulation_ex1

---
+++ python/dongbinna/chater2/simulation_ex1.py
@@ -0,0 +1,32 @@
+# NxN
+# (1, 1) (N, N) -> (0, 0) (N - 1, N - 1)
+# L R U D
+# 벗어나는 움직임은 무시해야 함.
+# 5
+# R R R U D D
+
+
+X_VECTOR = [0, 0, -1, 1]
+Y_VECTOR = [-1, 1, 0, 0]
+
+N = int(input())
+order = input().split()
+matrix = [[0] * N for _ in range(N)]
+print(order)
+
+dictionary = {'L': 0,'R': 1,'U': 2,'D': 3}
+
+currentLocation = (0, 0)
+for dir in order:
+    dx = X_VECTOR[dictionary[dir]]
+    dy = Y_VECTOR[dictionary[dir]]
+
+    nextX = currentLocation[0] + dx
+    nextY = currentLocation[1] + dy
+
+    print(nextX, nextY)
+
+    if 0 <= nextX < N and 0 <= nextY < N:
+        currentLocation = (nextX, nextY)
+
+print(currentLocation[0] + 1, currentLocation[1] + 1)
