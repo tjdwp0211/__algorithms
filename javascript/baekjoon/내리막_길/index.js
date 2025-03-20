3dcc14268a93ab3cbc287fea8b88324ba770c23e
tjdwp0211 <rlatjdwp0211@naver.com>
2025-03-20 22:24:07 +0900

    [baekjoon] 내리막 길
    - DP + (BFS, PriorityQueue)
    - Heap을 직접 짜본 경험이 빛을 약간 발휘했던 순간 ^___^
    - 물론 기존 구현 경험에 heapifyDown이 모든 요소를 고려하지 않았다는 문제가 있었지만,
    - 중요할 때가 아닌 지금 안 것이 상당히 기분 좋았다.
    - 시간이 굉장히 오래 걸리고 정답을 힐끔 봤기도 했음.
    - 추후 복습이 필요한 문제
    
    - 기존에 알던 DP 개념과 상당히 달랐다.
    - 내가 알던 DP는 Max 함수를 사용해서 하는 구조가 익숙했지만
    - 오늘 푼 내리막 길은 DP와 BFS, PriorityQueue와 같은 개념이 셖여 기존 구조와 좀 달라보인다.
    - 견문을 넓히게 해준 문제임.
    - DP + DFS로도 풀 수 있다는데, 복습 시에는 DP + DFS를 활용해서 풀어볼 예정.

---
+++ javascript/baekjoon/내리막_길/index.js
@@ -0,0 +1,135 @@
+const { OTHERS, LOCAL } = { OTHERS: "/dev/stdin", LOCAL: `${__dirname}/input.txt` };
+const fs = require("fs");
+
+/** ================================= START: CLASS 선언부 ================================= */
+class Node {
+  constructor(row, col, height) {
+    this.row = row;
+    this.col = col;
+    this.height = height;
+  }
+}
+
+class MaxHeap {
+  constructor() {
+    this.items = [];
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  swap(x, y) {
+    [this.items[x], this.items[y]] = [this.items[y], this.items[x]];
+  }
+
+  heapifyDown(head) {
+    while (true) {
+      let maxIndex = head;
+      const left = 2 * head + 1;
+      const right = 2 * head + 2;
+
+      if (left < this.size() && this.items[left].height > this.items[maxIndex].height) {
+        maxIndex = left;
+      }
+      if (right < this.size() && this.items[right].height > this.items[maxIndex].height) {
+        maxIndex = right;
+      }
+
+      if (maxIndex === head) break;
+      this.swap(head, maxIndex);
+      head = maxIndex;
+    }
+  }
+
+  heapifyUp(tail) {
+    while (Math.floor((tail - 1) / 2) >= 0) {
+      const parents = Math.floor((tail - 1) / 2);
+      if (this.items[parents].height < this.items[tail].height) {
+        this.swap(tail, parents);
+        tail = parents;
+      } else {
+        break;
+      }
+    }
+  }
+
+  heapPush(node) {
+    if (this.size() === 0) {
+      this.items.push(node);
+      return node;
+    }
+
+    this.items.push(node);
+    this.heapifyUp(this.size() - 1);
+    return node;
+  }
+
+  heapPop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    if (this.size() === 1) {
+      return this.items.pop();
+    }
+
+    const popped = this.items[0];
+    this.items[0] = this.items.pop();
+    this.heapifyDown(0);
+    return popped;
+  }
+}
+/** ================================= END: CLASS 선언부 ================================= */
+
+const DIR_ROW = [-1, 1, 0, 0];
+const DIR_COL = [0, 0, -1, 1];
+
+const [[ROW, COL], ...matrix] = fs
+  .readFileSync(process.platform == "linux" ? OTHERS : LOCAL)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((input) => input.split(" ").map(Number));
+
+const pq = new MaxHeap();
+const dp = Array.from({ length: ROW }, () => Array.from({ length: COL }, () => 0));
+
+pq.heapPush(new Node(0, 0, matrix[0][0]));
+dp[0][0] = 1;
+
+while (!pq.isEmpty()) {
+  const cur = pq.heapPop();
+
+  for (let d = 0; d < 4; d++) {
+    const [nextRow, nextCol] = [cur.row + DIR_ROW[d], cur.col + DIR_COL[d]];
+
+    if (outOfIndex(nextRow, nextCol)) {
+      continue;
+    }
+
+    if (canGo(cur.height, matrix[nextRow][nextCol])) {
+      if (isNotVisited(nextRow, nextCol)) {
+        pq.heapPush(new Node(nextRow, nextCol, matrix[nextRow][nextCol]));
+      }
+      dp[nextRow][nextCol] += dp[cur.row][cur.col];
+    }
+  }
+}
+console.log(dp[ROW - 1][COL - 1]);
+
+function outOfIndex(row, col) {
+  return row < 0 || ROW <= row || col < 0 || COL <= col;
+}
+
+function isNotVisited(row, col) {
+  return dp[row][col] === 0;
+}
+
+function canGo(cur, next) {
+  return next < cur;
+}
